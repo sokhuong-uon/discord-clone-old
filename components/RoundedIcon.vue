ed4747456c12d6bd43fb3fc454bad766f3e14264
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-08 20:27:23 +0700

    create button icon for discord servers

--- components/RoundedIcon.vue
+++ components/RoundedIcon.vue
@@ -1,14 +1,35 @@
 <template>
-	<div class="w-56 h-56">
-		<div class="rounded-full">
-			TB
-		</div>
+	<div @mouseover="isHover = true" @mouseleave="isHover = false" class="w-14 flex justify-center h-full rounded-larger overflow-hidden">
+		<a href="#">
+			<div :class="{'overflow-visible':isHover}" class="bg-transparent rounded-full overflow-hidden">
+				<img
+					class="w-14 h-14"
+					:src="server.gifUrl&&isHover ? server.gifUrl : server.imageUrl"
+					:alt="server.name" srcset=""
+				/>
+			</div>
+		</a>
 	</div>
 </template>
 
 <script>
 export default {
-
+	props: ['server'],
+	data() {
+		return{
+			isHover: false
+		}
+	},
+	mounted() {
+		if(!this.server){
+			console.log('Not a server');
+		}
+		console.log(this.server.name);
+		console.log(this.server.gifUrl);
+		if(this.server.gifUrl){
+			console.log('alright');
+		}
+	}
 }
 </script>
 
