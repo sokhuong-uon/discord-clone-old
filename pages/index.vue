ab2a1e8264c72d8293193f2b2a5aa4cdee191efc
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-08 14:44:32 +0700

    make layout more clear

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,34 @@
 <template>
 	<div class="">
+		<!--Server icons-->
+		<div>
+			<!--Direct message dm-->
+			<div></div>
+
+			<!--servers-->
+			<div></div>
+
+			<!--download app-->
+			<div></div>
+		</div>
+
+		<!--Server channels-->
 		<div></div>
-		<div></div>
-		<div></div>
+
+		<!--Main section-->
+		<div>
+			<!--Main section nav, sidebar-->
+			<div></div>
+
+			<!--Message section communicate-->
+			<div>
+				<!--Actual Message section communicate-->
+				<div></div>
+
+				<!--Members of the server or channel, online or offline-->
+				<div></div>
+			</div>
+		</div>
 	</div>
 </template>
 
