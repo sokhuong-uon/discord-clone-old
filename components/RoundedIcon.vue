d8fa710db59009139f71600f2f92b009aa9ae77c
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-08 22:56:05 +0700

    add the active little button

--- components/RoundedIcon.vue
+++ components/RoundedIcon.vue
@@ -1,14 +1,21 @@
 <template>
-	<div @mouseover="isHover = true" @mouseleave="isHover = false" class="w-14 flex justify-center h-full rounded-larger overflow-hidden">
-		<a href="#">
-			<div :class="{'overflow-visible':isHover}" class="bg-transparent rounded-full overflow-hidden">
-				<img
-					class="w-14 h-14"
-					:src="server.gifUrl&&isHover ? server.gifUrl : server.imageUrl"
-					:alt="server.name" srcset=""
-				/>
+	<div class="w-full flex justify-center items-center h-14 mb-3">
+		<div class="absolute flex w-4 h-14 -ml-16">
+			<div :class="{'h-8':isHover}" class="w-2 h-2 rounded-r-full bg-gray-100 -ml-1 my-auto transform"></div>
+		</div>
+		<div class="w-full flex justify-center mr-1">
+			<div @mouseover="isHover = true" @mouseleave="isHover = false" class="w-14 h-full rounded-larger overflow-hidden">
+				<a href="#">
+					<div :class="{'overflow-visible':isHover}" class="bg-transparent rounded-full overflow-hidden">
+						<img
+							class="w-14 h-14"
+							:src="server.gifUrl&&isHover ? server.gifUrl : server.imageUrl"
+							:alt="server.name" srcset=""
+						/>
+					</div>
+				</a>
 			</div>
-		</a>
+		</div>
 	</div>
 </template>
 
