23dd41dc42d2df73341155bd1a2bbf51b147eb48
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-08 20:25:29 +0700

    extend some spacing in tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,32 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
-  variants: {},
-  plugins: [],
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js'
-    ]
-  }
+	theme: {
+		extend: {
+			spacing: {
+				'14': '3.5rem',
+				'18': '4.5rem',
+				'72': '18rem',
+				'80': '20rem',
+				'84': '21rem',
+				'96': '24rem',
+			},
+			borderRadius:{
+				'larger':'1rem'
+			}
+		}
+	},
+	variants: {},
+	plugins: [],
+	purge: {
+		// Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+		enabled: process.env.NODE_ENV === 'production',
+		content: [
+		'components/**/*.vue',
+		'layouts/**/*.vue',
+		'pages/**/*.vue',
+		'plugins/**/*.js',
+		'nuxt.config.js'
+		]
+	}
 }
