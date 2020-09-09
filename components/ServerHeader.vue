a45e6dce1e3c1cd15ef8a775cd9a73b8ac1a2cea
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-09 17:42:47 +0700

    setup the structure of the component

--- components/ServerHeader.vue
+++ components/ServerHeader.vue
@@ -1,10 +1,22 @@
 <template>
-	<div></div>
+	<div class="w-60 h-12 bg-gray-900 bg-opacity-75">
+		<div class="w-full h-full px-4">
+			<!--If server if boosted-->
+			<div>
+			</div>
+			<!--server name-->
+			<h1></h1>
+
+			<!--menubar-->
+			<div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-
+	props: ['server']
 }
 </script>
 
