6afbc6d3ba972651d0ffe5fab1ee2c94fbc395dc
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-09 18:12:36 +0700

    make background lighter

--- components/ServerHeader.vue
+++ components/ServerHeader.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="w-60 h-12 bg-gray-900 bg-opacity-75">
+	<div class="w-60 h-12 bg-gray-800 bg-opacity-75">
 		<div class="w-full h-full px-4">
 			<!--If server if boosted-->
 			<div>
