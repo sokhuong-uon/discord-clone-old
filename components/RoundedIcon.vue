d33c4156f04761d7c572720a1bd23fc57cbd4253
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-09 14:54:26 +0700

    add more condition base on prop

--- components/RoundedIcon.vue
+++ components/RoundedIcon.vue
@@ -1,23 +1,37 @@
 <template>
-	<div class="relative w-full flex justify-center h-12 mb-3">
+	<div class="relative w-full flex justify-center h-12 mb-2">
 		<div class="absolute flex items-center w-3 h-12 left-0">
-			<span :class="{'h-8':isHover}" class="absolute w-6/12 h-3 bg-green-700 rounded-r-lg"></span>
+			<span :class="{'h-8':isHover}" class="absolute w-4/12 h-2 bg-green-700 rounded-r-lg"></span>
 		</div>
-		<div @mouseover="isHover = true" @mouseleave="isHover = false" class="w-12 flex justify-center">
-			<nuxt-link :class="{'':isHover}" class="" to="#">
+		<div @mouseover="isHover = true" @mouseleave="isHover = false" class="w-12 h-12 flex justify-start">
+			<nuxt-link v-if="type=='server'" :class="{'':isHover}" class="" to="#">
 				<img
-					class="w-12 h-12"
+					v-if="server.imageUrl"
+					class="w-12 h-12 rounded-full overflow-hidden"
 					:src="server.gifUrl&&isHover ? server.gifUrl : server.imageUrl"
 					:alt="server.name" srcset=""
 				/>
+				<div v-else class="w-12 h-12 flex justify-center items-center bg-gray-800 rounded-full overflow-hidden text-gray-200">
+					{{server.name.substring(0,4)}}
+				</div>
 			</nuxt-link>
+			<button v-if="type=='button'" class="w-12 h-12">
+				<div :class="{'bg-green-600':isHover}" class="w-12 h-12 bg-gray-800 flex justify-center items-center rounded-full overflow-hidden">
+					<svg :class="{'w-12 h-6 mt-1 mr-1 transform mx-auto':button.name=='Home'}" class="w-6 h-6 text-green-600" viewBox="0 0 24 24">
+						<path
+							:fill="isHover ? '#F7FAFC': 'currentColor'" fill-rule="evenodd" clip-rule="evenodd"
+							:d="button.svgPath"
+						/>
+					</svg>
+				</div>
+			</button>
 		</div>
 	</div>
 </template>
 
 <script>
 export default {
-	props: ['server'],
+	props: ['type', 'server', 'button'],
 	data() {
 		return{
 			isHover: false
@@ -27,11 +41,7 @@ export default {
 		if(!this.server){
 			console.log('Not a server');
 		}
-		console.log(this.server.name);
-		console.log(this.server.gifUrl);
-		if(this.server.gifUrl){
-			console.log('alright');
-		}
+
 	}
 }
 </script>
