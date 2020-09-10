cae3f998bd697a0737e54a22d75f17cf47b144e5
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-10 23:56:31 +0700

    change the server channel part

--- pages/index.vue
+++ pages/index.vue
@@ -45,7 +45,7 @@
 
                 <div class="smallScroleBar w-60 h-auto pb-18 flex flex-col overflow-auto bg-nightgray">
                     <div class="pr-1">
-                        <div class="h-3"></div>
+                        <div class="h-4"></div>
                         <div class="flex flex-col w-full">
                             <div v-for="(channel,i) in servers[3].channels" :key="i">
                                 <ChannelCard :channel="channel" />
@@ -54,12 +54,12 @@
 
                         <div draggable="true" v-for="(category, i) in servers[3].categories" :key="i" class="flex flex-col w-full">
                             <div class="w-full pt-4">
-                                <div class="w-58 my-px h-8 text-gray-700 flex justify-start items-center">
-                                    <svg class="mt-2" width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill="#4a5568" fill-rule="evenodd" clip-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
-                                    </svg>
-                                    <div>{{category.name}}</div>
-                                </div>
+                                <button class="w-58 h-6 rounded-lg text-gray-600 flex items-center">
+                                    <div class="w-4 h-4 flex justify-center items-center">
+                                        <svg class="w-2 h-2 transform hover:-rotate-90" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M19 9L12 16L5 9" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/> </svg>
+                                    </div>
+                                    <div class="uppercase text-xs">{{category.name}}</div>
+                                </button>
                                 <div class="flex flex-col w-full">
                                     <div v-for="(channel,j) in category.channels" :key="j">
                                         <ChannelCard :channel="channel" />
@@ -113,7 +113,8 @@ export default {
                     statement: "invisible"
                 }
             }],
-            buttons: [{
+            buttons: [
+                {
                     name: "Home",
                     svgPath: "M20.664 20s-.863-1.024-1.582-1.929c3.14-.88 4.34-2.833 4.34-2.833a13.75 13.75 0 01-2.758 1.405c-1.198.5-2.349.833-3.476 1.024-2.301.428-4.41.31-6.208-.024a20.231 20.231 0 01-3.525-1.024 14.1 14.1 0 01-1.75-.81c-.071-.047-.143-.07-.215-.118-.048-.024-.072-.048-.096-.072a8.414 8.414 0 01-.671-.405s1.15 1.905 4.195 2.81C8.198 18.929 7.312 20 7.312 20 2.014 19.833 0 16.381 0 16.381c0-7.667 3.452-13.88 3.452-13.88C6.904-.072 10.188 0 10.188 0l.24.285C6.113 1.524 4.123 3.405 4.123 3.405s.528-.286 1.415-.69c2.565-1.12 4.602-1.43 5.441-1.5.144-.024.264-.048.408-.048a20.422 20.422 0 014.843-.048c2.277.262 4.722.929 7.215 2.286 0 0-1.894-1.786-5.969-3.024l.336-.38s3.284-.072 6.736 2.5c0 0 3.452 6.213 3.452 13.88 0 0-2.038 3.452-7.336 3.619zM9.517 8.881c-1.366 0-2.445 1.19-2.445 2.643 0 1.452 1.103 2.643 2.445 2.643 1.367 0 2.445-1.19 2.445-2.643.024-1.452-1.078-2.643-2.445-2.643zm8.75 0c-1.366 0-2.445 1.19-2.445 2.643 0 1.452 1.103 2.643 2.445 2.643 1.367 0 2.445-1.19 2.445-2.643 0-1.452-1.078-2.643-2.445-2.643z"
                 },
