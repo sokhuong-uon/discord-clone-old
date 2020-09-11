1fda45f01fcefd34f95028880b632fd17c656891
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-11 18:46:32 +0700

    make it better

--- components/ChannelCard.vue
+++ components/ChannelCard.vue
@@ -4,10 +4,8 @@
         <span :class="{'h-2':channel.hasUnReadMessage > 0}" class="absolute w-1 bg-green-700 rounded-r-lg"></span>
     </div>
     <div :class="{'bg-gray-800':isHover, 'text-gray-600':channel.hasUnReadMessage === 0, 'text-green-500':channel.hasUnReadMessage > 0}" class="w-56 px-2 rounded-lg cursor-pointer flex items-center">
-        <div class="w-6 h-6 mr-2px">
-            <svg class="w-6 h-6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill="#4a5568" fill-rule="evenodd" clip-rule="evenodd" d="M8 4v4H4v2h4v4H4v2h4v4h2v-4h4v4h2v-4h4v-2h-4v-4h4V8h-4V4h-2v4h-4V4H8zm6 10v-4h-4v4h4z" />
-            </svg>
+        <div class="w-6 h-6 -ml-2px mr-1">
+            <svg width="24" height="24" class="text-gray-600 transform scale-80"><path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M5.887 21a.5.5 0 01-.493-.587L6 17H2.595a.5.5 0 01-.492-.586l.175-1A.5.5 0 012.77 15h3.58l1.06-6H4.005a.5.5 0 01-.492-.586l.175-1A.5.5 0 014.18 7h3.58l.637-3.587A.5.5 0 018.889 3h.984a.5.5 0 01.493.587L9.76 7h6l.637-3.587A.5.5 0 0116.889 3h.984a.5.5 0 01.493.587L17.76 7h3.405a.5.5 0 01.492.586l-.175 1A.5.5 0 0120.99 9h-3.58l-1.06 6h3.405a.5.5 0 01.492.586l-.175 1a.5.5 0 01-.492.414H16l-.637 3.587a.5.5 0 01-.492.413h-.984a.5.5 0 01-.493-.587L14 17H8l-.637 3.587a.5.5 0 01-.492.413h-.984zM9.41 9l-1.06 6h6l1.06-6h-6z"/></svg>
         </div>
         <div class="text-base font-semibold mb-px">
 			<p>{{channel.name}}</p>
