ac402bc6ed44a036ac36601e6fcecb90519295d9
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-12 11:55:03 +0700

    add class truncate to profile action section

--- components/UserInlineCard.vue
+++ components/UserInlineCard.vue
@@ -11,10 +11,10 @@
 			</div>
 		</div>
 	</div>
-	<div class="w-full h-10 text-gray-500 flex flex-col justify-center items-center">
+	<div class="w-41 h-10 text-gray-500 flex flex-col justify-center items-center">
 
-		<div class="w-full h-5">{{profile.userName}}</div>
-		<div class="w-full h-auto text-xs">{{profile.action.name}}</div>
+		<div class="w-41 h-5">{{profile.userName}}</div>
+		<div class="w-41 h-auto text-xs truncate overflow-hidden">{{profile.action.name}}</div>
 	</div>
 
 </div>
