92689e6f1fed93c236cf2363ca6bff48e05654e5
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-13 13:17:26 +0700

    made this component

---
+++ components/Chat.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="w-full h-auto mt-4 pr-12 pl-18 bg-gray-700">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, sit aliquid ipsa dignisdolor sit amet consectetur adipisicing elit. Inventore, sit aliquid ipsa dignissimos repellat ex! Saepe animi tempore tempora ducimus placeat, rerum eligendi at exercitationem alias ratione! Nemo autem maiores sit quos facilis? Fuga vel vero natus sunt enim. Alias commodi perferendis neque nam ad provident? Harum placeat corporis consequatur?
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
