cd92db4a7bf4e611d64c72a9a8186eee5603d017
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-17 17:02:27 +0700

    make the div and p that contain channle name turncate-able

--- components/ChannelCard.vue
+++ components/ChannelCard.vue
@@ -7,8 +7,8 @@
         <div class="w-6 h-6 -ml-2px mr-1">
             <svg width="24" height="24" class="text-gray-600 transform scale-80"><path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M5.887 21a.5.5 0 01-.493-.587L6 17H2.595a.5.5 0 01-.492-.586l.175-1A.5.5 0 012.77 15h3.58l1.06-6H4.005a.5.5 0 01-.492-.586l.175-1A.5.5 0 014.18 7h3.58l.637-3.587A.5.5 0 018.889 3h.984a.5.5 0 01.493.587L9.76 7h6l.637-3.587A.5.5 0 0116.889 3h.984a.5.5 0 01.493.587L17.76 7h3.405a.5.5 0 01.492.586l-.175 1A.5.5 0 0120.99 9h-3.58l-1.06 6h3.405a.5.5 0 01.492.586l-.175 1a.5.5 0 01-.492.414H16l-.637 3.587a.5.5 0 01-.492.413h-.984a.5.5 0 01-.493-.587L14 17H8l-.637 3.587a.5.5 0 01-.492.413h-.984zM9.41 9l-1.06 6h6l1.06-6h-6z"/></svg>
         </div>
-        <div class="text-base font-semibold mb-px">
-			<p>{{channel.name}}</p>
+        <div class="text-base font-semibold mb-px truncate">
+			<p class="truncate">{{channel.name}}</p>
 		</div>
 		<div v-if="isHover" class="ml-auto">
 			<div class="ml-auto flex items-center">
