6b6e4fddc4d3d76f96874ee1a5a64a1225c4e22f
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-20 18:32:51 +0700

    made this component as static

---
+++ components/SearchResultCard.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="h-10 px-2 hover:bg-gray-800 text-gray-500 flex items-center rounded">
+		<div class="w-5 h-full mr-2 flex items-center">
+			<div class="w-5 h-5">
+				<img src="https://cdn.discordapp.com/icons/694927446536880279/a59988d486e1ed6a3f3a1bd2e657ff0c.png?size=256" alt="" srcset="">
+			</div>
+		</div>
+		<div class="flex-1 h-full flex items-center">
+			<span class="text-gray-500 mr-2">Any Doubt</span>
+			<span class="text-teal-800">Hahaha</span>
+		</div>
+		<div class="h-full w-auto text-graydiscord">Any Doubt</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
