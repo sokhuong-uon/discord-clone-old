9172b01b8b0ad8a77ac02384b05f39424a136905
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-20 23:06:20 +0700

    remove z index

--- pages/_id.vue
+++ pages/_id.vue
@@ -65,7 +65,7 @@
 				<div class="w-auto h-4 ml-2 text-sm leading-none font-semibold tracking-tighter text-gray-500 truncate">Help related to Apollo in your Nuxt project. https://github.com/nuxt-community/apollo-module</div>
 			</div>
 
-			<div class="w-auto h-6 z-10 bg-nightgraylighter text-gray-500 flex-shrink-0 flex items-center justify-between overflow-hidden">
+			<div class="w-auto h-6 bg-nightgraylighter text-gray-500 flex-shrink-0 flex items-center justify-between overflow-hidden">
 				<div class="mx-2 flex items-center">
 					<button class="btn">
 						<svg class="w-6 h-6" aria-hidden="false" fill="none"><path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M18 9v5a3 3 0 003 3v1H3v-1a3 3 0 003-3V9a6 6 0 1112 0zm-6 12c-1.476 0-2.752-.81-3.445-2h6.89c-.693 1.19-1.97 2-3.445 2z"/></svg>
