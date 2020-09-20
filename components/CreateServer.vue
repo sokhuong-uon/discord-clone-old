8e2171d1654ae6fe88a557cc1ab4c05e501aa542
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-20 22:53:24 +0700

    a popup component just created

---
+++ components/CreateServer.vue
@@ -0,0 +1,24 @@
+<template>
+<div v-click-outside="externalClick" class="w-110 h-140 bg-green-500">
+
+</div>
+</template>
+
+<script>
+import vClickOutside from 'v-click-outside';
+export default {
+	directives: {
+    	clickOutside: vClickOutside.directive
+    },
+	data(){
+		return{
+
+		}
+	},
+    methods: {
+      	externalClick (event) {
+			this.$emit('externalClick', false);
+    	}
+    }
+}
+</script>
