c353e7316d27c281f4fd4229a08e64394d9c7c32
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-20 12:27:36 +0700

    popup component when user click on 'Find or start a conversation'

---
+++ components/FindConversation.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="w-90 h-auto bg-nightgray flex flex-col items-center justify-center">
+		<div class="w-full h-90 flex flex-col items-center justify-center">
+			<div class="w-auto min-w-30 max-w-full h-16 overflow-x-scroll bg-green-600" contenteditable="true"></div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
