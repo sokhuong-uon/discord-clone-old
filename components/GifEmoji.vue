f70d70edf264368582d0fe318a2d88095abd49d3
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-21 09:45:54 +0700

    for choosing gif or emoji

--- components/GifEmoji.vue
+++ components/GifEmoji.vue
@@ -1,9 +1,22 @@
 <template>
-<div></div>
+<div v-click-outside="externalClick" class="w-106 h-108 bg-pink-800"></div>
 </template>
 
 <script>
+import vClickOutside from 'v-click-outside';
 export default {
+	directives: {
+    	clickOutside: vClickOutside.directive
+    },
+	data(){
+		return{
 
+		}
+	},
+    methods: {
+      	externalClick (event) {
+			this.$emit('externalClick', false);
+    	}
+    }
 }
 </script>
