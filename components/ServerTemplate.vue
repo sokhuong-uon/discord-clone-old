ab9e3d34e53dbb1b88fdbe42c8db653e1ed27df2
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-09-21 18:09:08 +0700

    make this card for creat server component

---
+++ components/ServerTemplate.vue
@@ -0,0 +1,17 @@
+<template>
+<button class="w-100 h-18 flex items-center border rounded-lg border-gray-400 hover:shadow-md hover:bg-gray-200 focus:outline-none mb-2 flex-shrink-0">
+	<div class="w-12 h-12 ml-4 mr-2 my-2">
+		<img :src="imgUrl" alt="" srcset="">
+	</div>
+	<div class="text-lg font-bold">{{name}}</div>
+	<div class="ml-auto mr-4">
+		<img src="https://discord.com/assets/69a0ea5dbf79a129c81a0cb171b60b7a.svg" alt="" srcset="">
+	</div>
+</button>
+</template>
+
+<script>
+export default {
+	props: ['imgUrl', 'name']
+}
+</script>
