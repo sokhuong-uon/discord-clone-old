81a3e08c6c5a0fbdb11bbf6c1f52f9d4f07cd253
= <=>
2021-02-02 06:54:35 +0100

    test2

--- components/Chat.vue
+++ components/Chat.vue
@@ -1,6 +1,6 @@
 <template>
 
-<div v-if="currentChannelName=='123'" @mouseover="hoverChat = true" @mouseleave="hoverChat = false" class="relative flex-1 mt-4 min-w-0 pr-12 flex hover:bg-nightgray">
+<div v-if="currentChannelName=='1523'" @mouseover="hoverChat = true" @mouseleave="hoverChat = false" class="relative flex-1 mt-4 min-w-0 pr-12 flex hover:bg-nightgray">
 	<div class="w-12 h-12 mx-3 flex-shrink-0 flex items-center justify-center select-none">
 		<div class="w-10 h-10 rounded-full overflow-hidden">
 			<img :src="chat.author.avatarUrl" alt="" srcset="">
